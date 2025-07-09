b4a13cc37b5ac921613e7d5c790c91c9ec2f6400
jelf2006 <160517752+jelf2006@users.noreply.github.com>
2025-07-09 19:15:27 +0000

    mais um kata e explicando os demais

--- katas/highAndLow.js
+++ katas/highAndLow.js
@@ -3,12 +3,13 @@ return the highest and lowest number.*/
 
 // resolution 1:
 function highAndLow(n){
-  let x = n.split(" ").map(x => Number(x))
+  let x = n.split(" ").map(x => Number(x))//change the n to an array of numbers
   let b = x[0];
   let a = x[0];
-  for(let i = x.length - 1; i>0; --i){
-    (b<x[i])?b = x[i] : b;
-    (a>x[i])?a = x[i] : a;
+  for(let i = x.length - 1; i>0; --i){ /*it equal the number of times the loop is executed and
+     the number of elements in x*/
+    (b<x[i])?b = x[i] : b;//check the highter number and change the value of b to this
+    (a>x[i])?a = x[i] : a;//check the lowest number and change the value of a to this
   }
   return `${b} ${a}`
 }
--- katas/oddOrEven.js
+++ katas/oddOrEven.js
@@ -5,4 +5,8 @@ Give your answer as a string matching "odd" or "even".
 
 If the input array is empty consider it as: [0] (array with a zero).*/
 
-const oddOrEven = array => array.reduce((a,b)=> a + b, 0)%2 ? "odd" : "even"
+const oddOrEven = array => array.reduce((a,b)=> a + b, 0)%2 ? "odd" : "even" /*I have used 
+REDUCE, to sum all the numbers of the array, and %2 to check if the nunber was odd or even.
+1 = true and 0 = false*/
+
+//sorry, I'm learning english
