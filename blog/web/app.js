abae20643969b627321662f3880b7b53fc35e066
jelf2006 <160517752+jelf2006@users.noreply.github.com>
2025-09-12 21:34:20 +0000

    ideia de blog completa

--- blog/web/app.js
+++ blog/web/app.js
@@ -1,3 +1,9 @@
-$("button").on("click", function() {
-    alert("clicked")
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            alert("cliked")
+        })
+    })
+})
